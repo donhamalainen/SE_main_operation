51777e75c123cb7a796c25d81ae8c33c4761ac9c
don <hamalainen.don@gmail.com>
2024-09-27 13:25:44 +0300

    update

--- CSVParser.py
+++ CSVParser.py
@@ -1,38 +1,37 @@
 import os
+import csv
 
 # Open and read the CSV file
-def read_csv(filename):
+def read_csv(filename, output_filename):
     unique_projects = set() 
     unique_rows = []
 
     if os.path.exists(filename):
-        with open(filename, "r") as file:
-            header = file.readline().strip()
-            unique_rows.append(header)
+        with open(filename, "r", encoding="utf8") as file:
+            reader = csv.reader(file)
+            unique_rows.append(["Organization", "Project Name", "Github Link"])  # New headers
 
-            # Read each line in the CSV file
-            for line in file:
-                values = line.strip().split(",")
-                project_name = values[1]    
-                
-                
-                # Cheking
+            # Read each row in the CSV file
+            for row in reader:
+                organization = row[0]
+                project_name = row[1]
+
+                # Checking if the project is already added
                 if project_name not in unique_projects:
                     unique_projects.add(project_name)
                     # Generate GitHub link for the project
                     github_link = generate_github_links(project_name)
-                    values.append(github_link)
-                    unique_rows.append(",".join(values))
-                
-        # Writing  
-        with open(filename, "w") as output_file:
-            for row in unique_rows:
-                output_file.write(row + "\n")
+                    unique_rows.append([organization, project_name, github_link])
+        
+        # Writing the CSV File for unique projects with Github links
+        with open(output_filename, "w", encoding="utf8", newline='') as output_file:
+            writer = csv.writer(output_file)
+            # Write all unique rows to the new file
+            writer.writerows(unique_rows)
 
         return unique_projects
     else:
         print(f"File {filename} not found.")
-
 
 # We now that the links start https://github.com/apache/{something}
 def generate_github_links(project_name):
@@ -47,7 +46,10 @@ def generate_github_links(project_name):
 
 # Defining main function
 def main():
-    read_csv("sonar_measures.csv")
+    input_file = "sonar_measures.csv"
+    output_file = "parsed.csv"
+    read_csv(input_file, output_file)
+    print("Done")
     
 
 # Using the special variable 
